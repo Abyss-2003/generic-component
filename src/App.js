26bd17ec5dcf698134eb031f53495270399dee0c
Abhijeet Sahu <abhijeetsahu2003@gmail.com>
2023-12-28 12:25:22 +0530

    cards and button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 
 import './App.css';
-
+import Button from './Button'
+import Cards from './Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import img from './bg.jpg';
 function App() {
+  const handleCardClick=()=>{
+    alert('hello')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-       
-          Generic Component
-        
-      </header>
+      <Button size='lg' bgColor='red' btnTxt='Log in'/><hr></hr>
+      <Button size='md' btnTxt='Log in'/><hr></hr>
+      <Button size='sm' btnTxt='Log in'/><hr></hr>
+      <Cards title="Hello" desc="hi i am a developer" />
     </div>
   );
 }
